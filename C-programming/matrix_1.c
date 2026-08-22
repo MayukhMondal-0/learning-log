258debbdb5d111aedb34fc6a4b2f756c5fdc91fe
Mayukh <mayukh.ace.0.0@gmail.com>
2026-08-23 00:50:56 +0530

    Dynamic array implementation corrections

--- C-programming/matrix_1.c
+++ C-programming/matrix_1.c
@@ -1,24 +1,39 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct{
     int len;
-    int Dig_max;
+    int Digi_max;
     int *arr;
-}DynamicMatrix;
+}DynamicArray;
 
-void init(DynamicMatrix *arr){
-    arr->Dig_max=1;
-    arr->len=1;
+void init(DynamicArray *array){
+    array->arr=NULL;
+    array->Digi_max=1;
+    array->len=1;
 }
 
-void input(DynamicMatrix *Arr,int len_m,int dig){
-    Arr->arr=(int*)malloc((len_m)*sizeof(int));
-    Arr->len=len_m;
-    Arr->Dig_max=dig;
+void input(DynamicArray *Array,int len_m,int digi){
+    Array->arr=(int*)malloc((len_m)*sizeof(int));
+    if(Array->arr==NULL){
+        printf("Allocation Failed");
+        return;
+    }
+    Array->len=len_m;
+    Array->Digi_max=digi;
 }
 
-void append(DynamicMatrix *Arr,int new_len){
-    if(Arr->len<=new_len){
-        Arr->arr=0;
+void append(DynamicArray *Array,int new_len){
+    if(Array->len<new_len){
+        int *temp=realloc(Array->arr,new_len*sizeof(int));
+        if(temp==NULL){
+        printf("Re-allocation Failed");
+        return;
+        }
+        Array->arr=temp;
+        Array->len=new_len;
     }
 }
+
+// DynamicArray DynamicMatrix(){
+// }
