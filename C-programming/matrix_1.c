fd2e07d7e71e3d625e748ebfb25517264da49b6f
Mayukh <mayukh.ace.0.0@gmail.com>
2026-08-23 23:24:31 +0530

    dynamic implementation finished testing to be done

--- C-programming/matrix_1.c
+++ C-programming/matrix_1.c
@@ -7,13 +7,17 @@ typedef struct{
     int *arr;
 }DynamicArray;
 
+//initialize the list
+
 void init(DynamicArray *array){
     array->arr=NULL;
     array->Digi_max=1;
     array->len=1;
 }
 
-void input(DynamicArray *Array,int len_m,int digi){
+//initial memory allocation to list
+
+void create(DynamicArray *Array,int len_m,int digi){
     Array->arr=(int*)malloc((len_m)*sizeof(int));
     if(Array->arr==NULL){
         printf("Allocation Failed");
@@ -23,17 +27,77 @@ void input(DynamicArray *Array,int len_m,int digi){
     Array->Digi_max=digi;
 }
 
-void append(DynamicArray *Array,int new_len){
+//expand if the list is full
+
+void expand(DynamicArray *Array,int new_len,int n_digi){
     if(Array->len<new_len){
-        int *temp=realloc(Array->arr,new_len*sizeof(int));
+        int *temp=realloc(Array->arr,2*new_len*sizeof(int));
         if(temp==NULL){
         printf("Re-allocation Failed");
-        return;
+        return;                                     //need to add failure case
         }
         Array->arr=temp;
         Array->len=new_len;
+        Array->Digi_max=n_digi;
     }
 }
 
-// DynamicArray DynamicMatrix(){
-// }
+int DigiCheck(int num){
+    int digi=0;
+    while(num){
+        digi++;
+        num/=10;
+    }
+    return num;
+}
+
+//clears the buffer of a invalid output
+void BufferClear(){
+    int x;
+    while(x=getchar()!='\n'&&x!=EOF);
+}
+
+//render the array in real time
+void render(DynamicArray *array){
+    //  _     _
+    // | 2 2 5 |
+    // | 8 9 6 |
+    // |_1 4 9_|
+
+
+    
+}
+
+
+
+DynamicArray* DynamicMatrix(){
+    int n=-1;
+    DynamicArray *array;
+    init(array);
+    create(array,10,1);
+
+    for(int i=0;;i++){
+        if(scanf("%d",&n)!=1){
+            char c=getchar();
+            if(c=='x')return array;                           //input over
+            else if(c=='r'){
+               array->arr[i]=INT_MIN;                         //row change
+            }
+            else{
+                i--;
+                BufferClear();
+            }
+        }
+        else{
+            if(i<array->len){
+                array->arr[i]=n;
+                //render()
+            }
+            else{
+                expand(array,i,DigiCheck(n));
+                array->arr[i]=n;
+                //render()
+            }
+        }
+    }
+}
